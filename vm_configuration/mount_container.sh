de688eaab5eeef8b11aa976a3874fbb4f9d63140
Matt W <matt.weale@outlook.com>
2022-07-12 16:52:52 +0100

    Updated Container Mount

--- vm_configuration/mount_container.sh
+++ vm_configuration/mount_container.sh
@@ -3,6 +3,7 @@
 #	=============================
 #	NFS Mount Container
 #	=============================
+    echo "Mounting the Orbital Container to /nfsdata"
 
 #	Check Linux Distro for nfs utils install
     source /etc/os-release
@@ -21,13 +22,13 @@
     sudo mkdir -p /nfsdata
 
     if [ "$HOSTNAME" = "vm-orbital-data-collection" ]; then
-	    echo "Found Data Collection VM mounting /saorbital/raw-data"
+	    echo "Found Data Collection VM mounting /saorbital99/raw-data"
         sudo mount -o sec=sys,vers=3,nolock,proto=tcp saorbital.blob.core.windows.net:/saorbital/shared  /nfsdata
     elif [ "$HOSTNAME" = "vm-orbital-rtstps" ]; then
-	    echo "Found RT-STPS VM mounting /saorbital/rt-stps"
+	    echo "Found RT-STPS VM mounting /saorbital99/rt-stps"
         sudo mount -o sec=sys,vers=3,nolock,proto=tcp saorbital.blob.core.windows.net:/saorbital/shared  /nfsdata
     elif [ "$HOSTNAME" = "vm-orbital-ipopp" ]; then
-	    echo "Found IPOPP VM mounting /saorbital/ipopp"
+	    echo "Found IPOPP VM mounting /saorbital99/ipopp"
         sudo mount -o sec=sys,vers=3,nolock,proto=tcp saorbital.blob.core.windows.net:/saorbital/shared  /nfsdata
         cd /nfsd
     else
