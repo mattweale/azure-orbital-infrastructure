57a88e942bbc212cb40a68f1a50b387371dfc023
Matt W <matt.weale@outlook.com>
2022-07-13 10:41:32 +0100

    Testing var in subshells

--- vm_configuration/main_rtstps.sh
+++ vm_configuration/main_rtstps.sh
@@ -4,4 +4,4 @@ echo "From main_rtstps.sh the client.id of the MI is ${AQUA_MI_ID}"
 
 sudo ./mount_data_drive.sh
 sudo ./mount_container.sh
-sudo ./rtstps_install.sh
+sudo -E bash -c ./rtstps_install.sh
