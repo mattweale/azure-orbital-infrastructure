cff556ae18b54bb3fc2f51d3e7874562bc94aa9e
Matt W <matt.weale@outlook.com>
2022-07-13 09:48:28 +0100

    Testing moving vars

--- vm_configuration/main_rtstps.sh
+++ vm_configuration/main_rtstps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo $AQUA_MI_ID
+ echo "From rtstps_main.sh the client.id of the MI is ${AQUA_MI_ID}"
 
 sudo ./mount_data_drive.sh
 sudo ./mount_container.sh
--- vm_configuration/rtstps_install.sh
+++ vm_configuration/rtstps_install.sh
@@ -4,7 +4,7 @@
 #	RT-STPS Install
 #	=============================
 
-echo "The client.id of the MI is ${AQUA_MI_ID}"
+echo "From rtstps_install.sh the client.id of the MI is ${AQUA_MI_ID}"
 
 echo "Now on the RT-STPS Install"
 echo "First let's install azcopy"
